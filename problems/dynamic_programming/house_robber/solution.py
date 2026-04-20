7c7ca09ddfa53f6a907c646d2b746f535a460f20
nhatquang <tommyquanglowkey2011@gmail.com>
2026-04-21 00:09:14 +0700

    add(dp): house robber solution

--- problems/dynamic_programming/house_robber/solution.py
+++ problems/dynamic_programming/house_robber/solution.py
@@ -19,7 +19,7 @@ Examples:
         Explanation: Rob house 0 (1) and house 2 (3) = 4.
 
     Example 2:
-        Input:  nums = [2, 7, 9, 3, 1]
+        Input:  nums = [2, 7, 9, 3, 1] 
         Output: 12
         Explanation: Rob house 0 (2), house 2 (9), house 4 (1) = 12.
 
@@ -34,5 +34,19 @@ Hint (read only if stuck):
 
 
 def solution(nums: list[int]) -> int:
-    # TODO: implement your solution here
-    pass
+    # Handle base case
+    if len(nums) == 0:
+        return 0
+    if len(nums)==1:
+        return nums[0]
+    if len(nums)==2:
+        return max(nums[0],nums[1])
+    
+    # Implement main logic
+    prev2 = nums[0]
+    prev1 = max(nums[0],nums[1])
+    for i in range(2,len(nums)):
+        current = max(prev1,prev2+nums[i])
+        prev2 = prev1
+        prev1 = current
+    return prev1
